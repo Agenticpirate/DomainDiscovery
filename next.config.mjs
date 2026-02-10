8dc6a78f572cb2bce200b36d167fe6c559b2189e
Ravi Teja <pulligellaraviteja@gmail.com>
2026-02-10 23:13:11 +0530

    Add ESLint configuration to ignore during builds

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     process.env.REPLIT_DEV_DOMAIN,
     '127.0.0.1',
   ].filter(Boolean),
+    eslint: {
+          ignoreDuringBuilds: true,
+        },
 };
 
 export default nextConfig;
